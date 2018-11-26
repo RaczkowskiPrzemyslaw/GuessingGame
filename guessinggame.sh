72f7416bdf392d61f3534ce0270b724074e53276
RaczkowskiPrzemyslaw <przemyslaw@raczkowski.cba.pl>
2018-11-26 22:39:01 +0100

    Corrected guesing function

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,26 +1,24 @@
 flag_good=1
-
+numberOfFiles=$(ls | wc -l)
 function guess {
-echo "How many files are in current directory:"
-read response
-if [[ $response -eq {ls | wc -l} ]]
-then
-echo "Congratulations/! You've guessed correct number of files in this directory"
-flag_good=0
-elif [[ $response -lt {ls | wc -l} ]]
-then
-echo "Too low, try more, try again"
-flag_good=1
-else [[ $response -gt {ls | wc -l} ]]
-then 
-echo "Too much, try less, try again"
-flag_good=1
-fi
-}
-
+	echo "How many files are in current directory:"
+	read response
+		if [[ $response -eq $numberOfFiles ]]
+		then
+		echo "Congratulations! You've guessed correct number of files in this directory"
+		flag_good=0
+		elif [[ $response -lt $numberOfFiles ]]
+		then
+		echo "Too low, try more, try again"
+		flag_good=1
+		else [[ $response -gt $numberOfFiles ]] 
+		echo "Too much, try less, try again"
+		flag_good=1
+		fi
+	}
 while [[ $flag_good -ne 0 ]]
 do
- command guess
+guess
 done
 
 
